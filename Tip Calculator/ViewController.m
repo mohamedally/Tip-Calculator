637aee679c34a4d2269c7312ae7eb5d207ed52ac
Mohamed Ally Iddi <mudi@fb.com>
2019-06-25 15:57:54 -0700

    Implemented inputing bill amount, selecting tip percent and displaying tip and total amounts.

--- Tip Calculator/ViewController.m
+++ Tip Calculator/ViewController.m
@@ -9,6 +9,10 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UITextField *billField;
+@property (weak, nonatomic) IBOutlet UILabel *tipLabel;
+@property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+@property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
 
 @end
 
@@ -19,5 +23,24 @@
     // Do any additional setup after loading the view.
 }
 
+- (IBAction)onTaP:(id)sender {
+    NSLog(@"Hello");
+    [self.view endEditing:YES];
+}
+
+- (IBAction)onEdit:(id)sender {
+    double bill  = [self.billField.text doubleValue];
+    
+    NSArray *percentages = @[@(0.15), @(0.2), @(0.22)];
+    
+    double tipPercent = [percentages[self.tipControl.selectedSegmentIndex] doubleValue];
+    
+    double tip = tipPercent * bill;
+    double total = bill + tip;
+    
+    
+    self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", tip];
+    self.totalLabel.text = [NSString stringWithFormat:@"$%.2f", total];
+}
 
 @end
